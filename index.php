bb46afde928ff38ca82215e893a00a896c5ace78
seb-lju <120005073+seb-lju@users.noreply.github.com>
2023-01-17 15:34:00 +0200

    Skapade index.php
    Skapade index.php med navbar.
    Skapade knapp som tar användaren till register.php.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>TBD</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div class="navbar-container">
+		<div class="navbar">
+			<div class="navbar-left">
+				<a class="active" href="#home">Home</a></li>
+				<a href="#create">Create Bracket</a></li>
+				<a href="#brackets">Public Brackets</a></li>
+				<a href="#about">About</a></li>
+			</div>
+			<div class="navbar-right">
+				<button class="btn btn-login">Sign in</button>
+				<button onclick="location.href='/YP/register.php'" class="btn btn-register">Sign up</button>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
