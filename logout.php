7c9f9cfc6fb8c8df22a9bbebaa13e900d2fe9546
seb-lju <120005073+seb-lju@users.noreply.github.com>
2023-01-17 12:19:46 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,9 @@
 <?php  
-// startar sessionen 
 session_start();  
 
-// avslutar sessionen om användaren är inloggad  
 if (isset($_SESSION["logged_in"])) {  
     unset($_SESSION["logged_in"]);  
 }  
 
-// öppnar login.php på nytt
 header("Location: index.php");  
 ?> 
