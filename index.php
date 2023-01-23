eefaa4070bbeb2505d3eff146d2a2910d6967204
seb-lju <120005073+seb-lju@users.noreply.github.com>
2023-01-23 15:20:45 +0200

    Uppdaterade index.php
    Skapade funktionerna till inlogging och dark/light mode knappar

--- index.php
+++ index.php
@@ -4,6 +4,46 @@
 <meta charset="utf-8">
 <title>TBD</title>
 <link rel="stylesheet" href="style.css">
+	<script>
+		function toggleDark() {
+			var btn = document.getElementById("dark");
+			var body = document.body;
+			if (body.classList.contains("dark-theme")){
+				body.classList.remove("dark-theme");
+				btn.innerHTML = "Dark mode";
+			}
+			else{
+				body.classList.add("dark-theme");
+				btn.innerHTML = "Light mode";
+			}
+		}
+		
+		function dropLogin() {
+			var loginForm = document.getElementById("loginForm") 
+			if(loginForm.classList.contains("show")){
+				loginForm.style.display = "none";
+				loginForm.classList.remove("show");
+			}
+			else {
+				loginForm.style.display = "block";
+				loginForm.classList.add("show");
+			}
+		}
+		
+		window.onclick = function(event) {
+			if (!event.target.matches('.btn-login')) {
+				var dropdowns = document.getElementsByClassName("dropdown-content");
+				var i;
+				for (i = 0; i < dropdowns.length; i++) {
+					var openDropdown = dropdowns[i];
+					if (openDropdown.classList.contains('show')) {
+						openDropdown.classList.remove('show');
+						openDropdown.style.display = "none";
+					}
+				}
+			}
+		}
+	</script>
 </head>
 
 <body>
@@ -16,8 +56,33 @@
 				<a href="#about">About</a></li>
 			</div>
 			<div class="navbar-right">
-				<button class="btn btn-login">Sign in</button>
+				
+				
+				<div class="dropdown">
+					<button class="btn btn-login" onclick="dropLogin()">Sign in</button>
+					<div id="loginForm" class="dropdown-content">
+						<form action="" name="newuser" method="post">
+							<h3>Sign up</h3> 
+							<p>
+								<label for="username">Username:</label><br />
+								<input type="text" name="username" id="username" size="40" value="" required>
+							</p>
+							<p>
+								<label for="password">Password:</label><br />
+								<input type="password" name="password" id="password" size="40" value="" required>
+							</p>
+							<p>
+								<input type="submit" class="send" name="submit" id="submit" value="Sign up">
+							</p>
+						</form>
+					</div>
+				</div>
+				
+				
 				<button onclick="location.href='/YP/register.php'" class="btn btn-register">Sign up</button>
+				<div class="dark-mode">
+					<button id="dark" onclick="toggleDark()">Dark mode</button>
+				</div>
 			</div>
 		</div>
 	</div>
