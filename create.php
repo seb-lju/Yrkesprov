bdb255cb0ed07cf98d6fb9cfa2e15ad2b1f09bea
seb-lju <120005073+seb-lju@users.noreply.github.com>
2023-02-07 15:12:15 +0200

    Update create.php
    Skapade konsolformat och generator för konsolskapning.
    Båda är "Work in progress".

--- create.php
+++ create.php
@@ -2,15 +2,6 @@
   session_start();
   ob_start();
 
-if(!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true){
-	?>
-		<script>
-			function guestSettings() {
-				body.classList.add("guestSettings");
-			}
-		</script>
-	<?
-}
 ?>
 <!doctype html>
 <html>
@@ -19,6 +10,7 @@ if(!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true){
 <title>Create Bracket</title>
 <link rel="stylesheet" href="style.css">
 	<script>
+		
 		function toggleDark() {
 			var btn = document.getElementById("dark");
 			var body = document.body;
@@ -136,30 +128,299 @@ if(!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true){
 		</div>
 	</div>
 
-	<div class="object">
 		<div class="create">
-		  <h1>Create Bracket</h1><br>
-			  <h2>Some text some text some text some text..</h2><br>
+		  <form action="" method="post">
+			<h2>Create Bracket</h2><br/>
+			<p>
+				<label for="bracket_name">Bracket name: <span style="color:red;">*</span></label>
+				<input type="text" name="bracket_name" required>
+			</p><br/>
 
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
+			  <p>
+				<label for="bracket_format">Format: <span style="color:red;">*</span></label>
+				<select name="format" required>
+				  <option value="single">Single Elimination</option>
+				  <option value="double">Double Elimination</option>
+				  <option value="rr">Round Robin</option>
+				  <option value="swiss">Swiss</option>
+				</select>
+			</p><br/>
+			<p>
+				<label for="bracket_name">Sport: <span style="color:red;">*</span></label>
+				<select name="sport" required>
+				  <?php
+					require "dbconn.php";
+					$sql = "SELECT * FROM sport";
+					$result = $dbconn->query($sql);
+					
+					while ($row = $result->fetch_assoc()){
+				  ?>
+						<option value="<?php echo $row["sport_id"];?>">
+						  <?php echo $row["sport"];?>
+						</option>
+				  <?php
+					}
+					$dbconn->close();
+				  ?>
+				</select>
+			</p><br/>
+			  
+			  <input type="submit" value="Generate">
+		  </form>
+			<div class="participants ui-sortable">
+				<div class="participant" id="participant-1">
+					<div class="participant-handle ui-sortable-handle"></div>
+					<div class="participant-seed ui-sortable-handle">1</div>
+					<div class="participant-name">
+						<input class="input-participant" type="text" name="participant-1" maxlength="35" value="Team 1">
+					</div>
+					<div class="participant-delete">
+						<div class="icon icon-close"></div>
+					</div>
+				</div>
+				<div class="participant" id="participant-2">
+					<div class="participant-handle ui-sortable-handle"></div>
+					<div class="participant-seed ui-sortable-handle">2</div>
+					<div class="participant-name">
+						<input class="input-participant" type="text" name="participant-2" maxlength="35" value="Team 2">
+					</div>
+					<div class="participant-delete">
+						<div class="icon icon-close"></div>
+					</div>
+				</div>
+				<div class="participant" id="participant-3">
+					<div class="participant-handle ui-sortable-handle"></div>
+					<div class="participant-seed ui-sortable-handle">3</div>
+					<div class="participant-name">
+						<input class="input-participant" type="text" name="participant-3" maxlength="35" value="Team 3">
+					</div>
+					<div class="participant-delete">
+						<div class="icon icon-close"></div>
+					</div>
+				</div>
+				<div class="participant" id="participant-4">
+					<div class="participant-handle ui-sortable-handle"></div>
+					<div class="participant-seed ui-sortable-handle">4</div>
+					<div class="participant-name">
+						<input class="input-participant" type="text" name="participant-4" maxlength="35" value="Team 4">
+					</div>
+					<div class="participant-delete">
+						<div class="icon icon-close">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="add-buttons">
+				<div class="button button-shuffle-seeds" title="Shuffle Seeds">
+					<div class="icon icon-shuffle"></div>
+				</div>
+				<div class="button button-add-participant">+ Add Participant</div>
+			</div>
 
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
-			  <p>Some text some text some text some text..</p><br>
 		</div>
-	</div>
+		<div class="bracket">
+			<div class="round round-16">
+				<div class="game" id="game-8">
+					<div class="slot slot-top">
+						<div class="slot-seed">1</div>
+						<div class="slot-name">Team 1</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">16</div>
+						<div class="slot-name">Team 16</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game" id="game-9">
+					<div class="slot slot-top">
+						<div class="slot-seed">9</div>
+						<div class="slot-name">Team 9</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">8</div>
+						<div class="slot-name">Team 8</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game" id="game-10">
+					<div class="slot slot-top">
+						<div class="slot-seed">5</div>
+						<div class="slot-name">Team 5</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">12</div>
+						<div class="slot-name">Team 12</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game" id="game-11">
+					<div class="slot slot-top">
+						<div class="slot-seed">13</div>
+						<div class="slot-name">Team 13</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">4</div>
+						<div class="slot-name">Team 4</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-12">
+					<div class="slot slot-top">
+						<div class="slot-seed">3</div>
+						<div class="slot-name">Team 3</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">14</div>
+						<div class="slot-name">Team 14</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-13">
+					<div class="slot slot-top">
+						<div class="slot-seed">11</div>
+						<div class="slot-name">Team 11</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">6</div>
+						<div class="slot-name">Team 6</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-14">
+					<div class="slot slot-top">
+						<div class="slot-seed">7</div>
+						<div class="slot-name">Team 7</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">10</div>
+						<div class="slot-name">Team 10</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-15">
+					<div class="slot slot-top">
+						<div class="slot-seed">15</div>
+						<div class="slot-name">Team 15</div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed">2</div>
+						<div class="slot-name">Team 2</div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+			</div>
+			<div class="connectors connectors-8">
+				<div class="connector"></div>
+				<div class="connector"></div>
+				<div class="connector connector-right"></div>
+				<div class="connector connector-right"></div>
+			</div>
+			<div class="round round-8">
+				<div class="game" id="game-4">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game" id="game-5">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-6">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-7">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+			</div>
+			<div class="connectors connectors-4">
+				<div class="connector"></div>
+				<div class="connector connector-right"></div>
+			</div>
+			<div class="round round-4">
+				<div class="game" id="game-2">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+				<div class="game game-right" id="game-3">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+			</div>
+			<div class="connectors connectors-2">
+				<div class="connector"></div>
+			</div>
+			<div class="round round-2">
+				<div class="game" id="game-1">
+					<div class="slot slot-top">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+					<div class="slot slot-bot">
+						<div class="slot-seed"></div>
+						<div class="slot-name"></div>
+						<div class="slot-score"></div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 	<div class="footer">
 		<div>
